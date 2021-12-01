5a64331bc0dbba90116a2c9e382ea40704bae346
JUSSIAR <klokov.stas.022@mail.ru>
2021-12-01 20:55:54 +0300

    almost ready

--- HW4/src/source.c
+++ HW4/src/source.c
@@ -1,32 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct X {
-    int x;
-    char y;
+struct Auto {
+    int capacity;
+    int transport;
+    int additional;
+    double spending;
+    double distance;
 };
 
-extern char sort(struct X*);
+//extern void sort(struct Auto*, int);
+extern double func(double*, int);
 
-int main()
-{
-    printf("\n\n%d\n\n", sizeof (struct X));
+const char* names[3] = {"Car", "Bus", "Truck"};
 
-    struct X* arr = malloc(sizeof(struct X) * 5);
-    char* s = "bdcea";
-    int i[5] = {2, 4, 3, 5, 1};
-    for (int g = 0; g < 5; ++g) {
-        arr[g].x = i[g];
-        arr[g].y = s[g];
+void input(FILE* in, int size, struct Auto* container) {
+    for (int item = 0; item < size; item++) {
+        scanf("%d ", &container[item].capacity);
+        scanf("%lf ", &container[item].spending);
+        scanf("%d ", &container[item].transport);
+        scanf("%d\n", &container[item].additional);
     }
+}
 
-    printf("\n\n%c\n\n", sort(arr));
+void output(FILE* out, int size, struct Auto* container) {
+    for (int item = 0; item < size; item++) {
+        printf(
+            "%s maxDistance = %lf\n",
+            names[container[item].transport - 1],
+            func(&container[item].spending, container[item].capacity)
+        );
+    }
+}
 
-//    printf("\n\n\n");
-//    for (int g = 0; g< l; g++) {
-//        printf("%c", s[g]);
-//    }
-//    printf("\n\n\n");
+void calc(struct Auto* container, int size) {
+    for (int item = 0; item < size; item++) {
+        container[item].distance = func(&container[item].spending, container[item].capacity);
+    }
+}
+
+void mov(struct Auto* from, struct Auto* to) {
+    to->capacity = from->capacity;
+    to->transport = from->transport;
+    to->additional = from->additional;
+    to->spending = from->spending;
+    to->distance = from->distance;
+}
+
+void sort(struct Auto* container, int size) {
+    for (int i = 1; i < size; i++) {
+        for (int j = i; j > 0 && (container[j].distance < container[j - 1].distance); j--) {
+            struct Auto current;
+            mov(&container[j], &current);
+            mov(&container[j - 1], &container[j]);
+            mov(&current, &container[j - 1]);
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    int size;
+    struct Auto* container = NULL;
+
+//    FILE* in = fopen("../tests/test-suits/generated/test1.txt", "r");
+//    FILE* out = fopen("../results/generated/result1.txt", "w");
+    FILE* in = fopen(argv[1], "r");
+    FILE* out = fopen(argv[2], "w");
+
+    fscanf(in, "%d\n", &size);
+    container = malloc(sizeof(struct Auto) * size);
+    input(in, size, container);
+
+    calc(container, size);
+    sort(container, size);
+
+    output(out, size, container);
+    free(container);
+    container = NULL;
+
+    fclose(in);
+    fclose(out);
 
     return 0;
 }
+
+// terminal params example:
+// ../tests/test-suits/generated/test1.txt ../results/generated/result1.txt
